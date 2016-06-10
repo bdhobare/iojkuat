53c0e8cc149e44a469cc36ca9026e48e782587ee
bdhobare <bdhobare@gmail.com>
2016-06-10 18:35:42 +0300

    Added scoped access

---
+++ mobile/src/main/java/gdg/iojkuat/scopedaccess/ScopedActivity.java
@@ -0,0 +1,22 @@
+package gdg.iojkuat.scopedaccess;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import gdg.iojkuat.R;
+
+public class ScopedActivity extends AppCompatActivity {
+    private static final String FRAGMENT_TAG = ScopedDirectoryAccessFragment.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scoped);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, ScopedDirectoryAccessFragment.newInstance(), FRAGMENT_TAG)
+                    .commit();
+        }
+
+    }
+}
